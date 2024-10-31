8bdda558d4bf89de9d749e2aaf63853a4dbbd6fc
“katerinav44” <katerina.vovk@mail.utoronto.ca>
2024-10-31 11:36:16 -0400

    modified:   data_test.py 	new file:   heuristic.py

--- data_test.py
+++ data_test.py
@@ -1,5 +1,6 @@
 from run_factory import *
 from k_median_single_fact import *
+from heuristic import *
 import json
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,7 +20,7 @@ for i in range (len(data['factory_locations'])):
 xmin=0
 xmax=600
 ymin=750
-ymax=1750
+ymax=1250
 bbox=[(xmin,ymin), (xmax,ymin), (xmax,ymax), (xmin,ymax), (xmin,ymin)]
 #remove bays not in the box
 bays_test1=[]
@@ -29,20 +30,25 @@ for i in range(len(bays)):
     if x>=xmin and x<=xmax and y>=ymin and y<=ymax:
         bays_test1.append((x,y))
 
-t_finish_test1=test_factories(100, facts, bays_test1, True)
+#t_finish_test1=test_factories(10, facts, bays_test1, True)
 #get index of min time factory
 #index_min = np.argmin(t_finish_test1)
-min_value = min(t_finish_test1)
-min_fact_test1=[facts[i] for i, x in enumerate(t_finish_test1) if x == min_value]
+#min_value = min(t_finish_test1)
+#min_fact_test1=[facts[i] for i, x in enumerate(t_finish_test1) if x == min_value]
 
 n_vehicles=10
-min_fact_test2=k_median_single_factory(bays_test1, facts, n_vehicles)
+#min_fact_test2=k_median_single_factory(bays_test1, facts, n_vehicles)
+
+n_factories=3
+min_fact_test3, time_test3=heuristic_solution(bays_test1, facts, n_factories)
 
 #plot bays and factories 
 plt.figure(figsize=(10,15))
 plt.scatter(*zip(*bays), marker='.', color='k', label='bays')
 plt.scatter(*zip(*facts), marker='x', color='red', label='factories')
-plt.scatter(*zip(*min_fact_test2), marker='o', s=70,color='green', label='optimal factory location')
+#plt.scatter(*zip(*min_fact_test2), marker='o', s=70,color='green', label='optimal factory location k-median')
+#plt.scatter(*zip(*min_fact_test1), marker='x', s=50,color='purple', label='optimal factory location objective func')
+plt.scatter(*zip(*min_fact_test3), marker='o', s=70, color='magenta', label='Optimal factory locations heuristic method 3 factories')
 plt.plot(*zip(*bbox), color='blue', label='bounding box for test')
 plt.xlabel('x')
 plt.ylabel('y')
---
+++ heuristic.py
@@ -0,0 +1,41 @@
+import itertools
+import math
+import numpy as np
+from run_factory import *
+import json
+
+
+def calculate_time(factory_locations, bays):
+    total_time = 0
+    factory_assignments = {factory: [] for factory in factory_locations}
+
+    # Assign each bay to the closest factory
+    for bay in bays:
+        closest_factory = min(factory_locations, key=lambda factory: dist(factory, bay))
+        factory_assignments[closest_factory].append(bay)
+    
+    constr_times=[]
+    for factory, assigned_bays in factory_assignments.items():
+        constr_times.append(run_factory(factory, assigned_bays, 3, True))
+    #Since the factories are building at the same time, the time it would take to build the whole site corresponds to the maximum time among each construction group
+    return max(constr_times)
+
+def heuristic_solution(bays, factory_locations, n_factories):
+    best_time = float('inf')
+    best_assignment = None
+
+    # Generate all combinations of two factory locations
+    for factory_group in itertools.combinations(factory_locations, n_factories):
+        time = calculate_time(factory_group, bays)
+        if time < best_time:
+            best_time = time
+            best_assignment = factory_group
+
+    print("Best Factory Locations:", best_assignment)
+    print("Total Time Required:", best_time)
+
+    return best_assignment, best_time
+
+
+
+
