201132636ac9dbbd062d326e75c8d301f6c3bdcb
nkusanda1 <natkusanda@gmail.com>
2024-11-10 19:21:21 -0500

    k-median attempt at using run factory

--- k_median_single_fact.py
+++ k_median_single_fact.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt
 def k_median_single_factory(bays, facts, n_vehicles, k):
     print("start lp")
     bays_list=bays
+    print(len(bays_list))
     facts_list=facts
     bays = np.array(bays_list)
     facts = np.array(facts_list) #the most annoying way of doing this I know
@@ -29,18 +30,22 @@ def k_median_single_factory(bays, facts, n_vehicles, k):
     # X and Y decision variables
     X = pl.LpVariable.dicts("bay_factory", indices = (range(len(bays)), range(len(facts))), lowBound=0, upBound=1, cat='Binary')
     Y = pl.LpVariable.dicts("factory_chosen", indices = range(len(facts)), lowBound=0, upBound=1, cat='Binary')
+    # define z for (3)
     #z = pl.LpVariable("max_run_fact_time")
 
     # === Objective Function ===
     # Minimize k-median distance as a heuristic for time
     problem += (
+        # (0) Standard using sum of manhattan distances
         sum(sum(spd.cityblock(bays[i], facts[j]) * X[i][j] for j in range(n_facts)) for i in range(n_bays))
 
-        # using run factory - this only works for single factory
+        # (1) using run factory - this only works for single factory
         # sum(d[j] * Y[j] for j in range(n_facts))
 
-        # (Attempt 2 - run_factory k > 1) sum(run_factory(facts_list[j], [bays_list[i] for i in range(n_bays) if X[i][j] == 1], 1, True) * Y[j] for j in range(n_facts)) 
-        # (Attempt 3) z
+        # (Attempt 2 - run_factory k > 1) 
+        #sum(run_factory(facts_list[j], [bays_list[i] for i in range(n_bays) if X[i][j] == 1], 1, True) for j in range(n_facts)) 
+        # (3) using run factory on a selection of bays
+        #z
     ), "k_median"
 
     # === Constraints ===
@@ -50,7 +55,8 @@ def k_median_single_factory(bays, facts, n_vehicles, k):
     problem += sum(Y[j] for j in range(n_facts)) == k # k factories chosen
 
     for j in range(n_facts):
-        # (Attempt 3) problem += run_factory(facts_list[j], [bays_list[i] for i in range(n_bays) if X[i][j] == 1], 1, True) * Y[j] <= z
+        # Define constraint for (3)
+        #problem += run_factory(facts_list[j], [bays_list[i] for i in range(n_bays) if X[i][j] == 1], 1, True) <= z
         problem += sum(X[i][j] for i in range(n_bays)) <= Y[j] * n_bays # each factory j services less than all bays
 
     # === Solve the problem ===
@@ -108,10 +114,10 @@ if __name__ == "__main__":
         facts.append((data['factory_locations'][i]['x'], data['factory_locations'][i]['y']))
 
     #Since there are 20k panels we will test using a bounding box:
-    xmin=200
+    xmin=0
     xmax=600
-    ymin=1000
-    ymax=1250
+    ymin=250
+    ymax=1750
     bbox=[(xmin,ymin), (xmax,ymin), (xmax,ymax), (xmin,ymax), (xmin,ymin)]
     #remove bays not in the box
     bays_test1=[]
